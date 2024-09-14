91ed6868907d10ac07c73340360ada2ab3723c10
abhishek <abhishek.amber@nirogstreet.in>
2024-09-14 17:59:31 +0530

    fix

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,3 +1,3 @@
-// export const API = "https://chat-app-0c6p.onrender.com";
+export const API = "https://chat-app-0c6p.onrender.com";
 
-export const API = "http://localhost:5000";
+// export const API = "http://localhost:5000";
